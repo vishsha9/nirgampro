4a5a00d39323d2b5109782a4021d714118bf9a2a
glorywh <glorywh@6e2dba06-825a-11de-a3c6-f150bf15ed86>
2009-09-27 12:53:34 +0000

    log cont. for the last log notation, parse part in tracker and some other assistant function scattered in codes could be moved out together. Maybe a util directory?

---
+++ nirgampro/trunk/src/constants.h
@@ -0,0 +1,19 @@
+#ifndef __CONSTANT_H__
+#define  __CONSTANT_H__
+
+//////////////////////////////////////////////////////////////////////////
+// old constant parameter
+// buffer parameters
+#define	BUF_BITSIZE	4
+/// maximum buffer depth
+#define	MAX_NUM_BUFS	16
+
+/// number of virtual channels
+#define NUM_VCS 1
+#define VCS_BITSIZE 3 // 0~7
+
+// parameters for bitwidth
+#define ADDR_SIZE 32
+//////////////////////////////////////////////////////////////////////////
+
+#endif
